2cdc616086544cbb5209b122a82194a7709674cc
philogag <philogag@qq.com>
2021-07-14 12:28:35 +0800

    docker in docker

--- tools/install-docker.sh
+++ tools/install-docker.sh
@@ -10,7 +10,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=${CORE}] https://download.docker.com/linux/ubuntu focal stable"
 
 apt-get update
-apt-get install -y docker-ce
+apt-get install -y docker-ce-cli
 
-service docker start
 docker version
--- tools/run-in-woker.sh
+++ tools/run-in-woker.sh
@@ -3,6 +3,7 @@
 ## 本脚本用于配置外层虚拟指令集docker，并传入构建参数
 
 docker run -it \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     -v /opt/export:/opt/export \
     -v $PWD/tools:/opt/tools \
     -v $PWD/builds:/opt/builds \
