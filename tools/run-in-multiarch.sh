4001eb9505f257589d3652807d3fdbbfa505d276
philogag <philogag@qq.com>
2021-07-14 12:51:43 +0800

    update inner docker run

--- tools/run-in-multiarch.sh
+++ tools/run-in-multiarch.sh
@@ -5,10 +5,10 @@
 bash /opt/tools/install-docker.sh
 
 uname -a
-
+ls /opt/builds
  # 运行内层
 docker run -it \
     -v /opt/export:/opt/export \
     -v /opt/builds:/opt/builds \
     python:${PY}-buster \
-    bash /opt/builds/${LIB}.sh
+    cd /opt/builds && bash ./${LIB}.sh
