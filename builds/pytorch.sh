f09d3e384204d1d8b640d4676b018dbd93bc26fb
Philogag <44018455+Philogag@users.noreply.github.com>
2022-01-15 23:40:29 +0800

    Update pytorch.sh

--- builds/pytorch.sh
+++ builds/pytorch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget https://download.pytorch.org/whl/cu113/torch-1.10.1%2Bcu113-cp37-cp37m-win_amd64.whl
+wget -q https://download.pytorch.org/whl/cu113/torch-1.10.1%2Bcu113-cp37-cp37m-win_amd64.whl
 
+mkdir /opt/export/
 cp *.whl /opt/export/
