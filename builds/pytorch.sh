7554565b98cfa2848ae22da6942b315d6a726fc8
Philogag <44018455+Philogag@users.noreply.github.com>
2022-01-15 23:46:59 +0800

    Update pytorch.sh

--- builds/pytorch.sh
+++ builds/pytorch.sh
@@ -4,3 +4,7 @@ wget -q https://download.pytorch.org/whl/cu113/torch-1.10.1%2Bcu113-cp37-cp37m-w
 
 mkdir /opt/export/
 cp *.whl /opt/export/
+
+cd /opt/export/
+find . -type f -name "*.whl" ! -path "./zips/*" -exec zip -r -0 -s 1g ./zips/{}.zip {} \;
+ls /opt/export/zips
